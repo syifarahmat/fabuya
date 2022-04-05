f98480043e7d711574c3e2844cbf7e1f1055ebd9
HanzCEO <hanzhaxors@gmail.com>
2022-04-05 07:50:07 +0700

    chore(build): Add `build.sh` file

---
+++ build.sh
@@ -0,0 +1,13 @@
+echo Please wait, this might takes minutes
+
+# build Baileys
+echo NOTE: You need to run \`npm run init:baileys\` before running this
+npm run build:baileys
+
+# Copy Baileys runtime dependencies
+cp -rf ./Baileys/WAProto ./WAProto
+cp -rf ./Baileys/WABinary ./WABinary
+cp -rf ./WABinary/Constants.js ./baileys-lib/Constants.js
+cp -rf ./Baileys/WASignalGroup ./WASignalGroup
+
+# TODO: update Baileys and see the effect
