3f5d149f4fdd3bc8e9ea86367e9fd6375403c112
HanzCEO <hanzhaxors@gmail.com>
2022-04-09 08:15:29 +0700

    feat(utils): Add `getWhatsappVersion` function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,22 @@
+const axios = require('axios');
+
+/**
+ * Instead of relying on Baileys' method,
+ * lets fetch directly from whatsapp
+ */
+async function getWhatsappVersion() {
+	let res = await axios.get("https://web.whatsapp.com/check-update?version=2.2210.9&platform=web");
+
+	if (res.status == 200) {
+		let data = JSON.parse(res.data);
+		let version = data.currentVersion.split('.');
+		return version.map(n => Number(n));
+	}
+
+	// Fallback is set manually
+	return [2, 2210, 9];
+}
+
 function normalizePhoneNumber(phone) {
 	// Replace non numbers
 	let normalized = phone.replace(/\D/g, '');
