654e60666c11fe640be92abee7d5c07bebf09369
HanzCEO <hanzhaxors@gmail.com>
2022-04-27 15:15:14 +0700

    feat(Chat): Add `.lastMessage` and `toJSON`

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,8 +1,12 @@
 class Chat {
 	constructor() {
 		this.me = undefined;
+		this.store = undefined;
 		this.id = '';
 		this.name = '';
+		this.isGroup = false;
+
+		this.properties = ['id', 'name', 'isGroup'];
 	}
 
 	async send(msg) {
@@ -10,6 +14,23 @@ class Chat {
 	}
 
 	// TODO: Get Last Message
+	get lastMessage() {
+		if (this.store === undefined) {
+			// We require message store to retrieve this
+			return null;
+		}
+
+		let msgs = this.store.getChatLatestMessages(this.id, 10);
+		return msgs[msgs.length-1];
+	}
+
+	toJSON() {
+		let dict = {};
+		for (const prop of this.properties) {
+			dict[prop] = this[prop];
+		}
+		return dict;
+	}
 }
 
 module.exports = Chat;
--- src/Chat/Group.js
+++ src/Chat/Group.js
@@ -3,9 +3,13 @@ let Chat = require('./Chat');
 class Group extends Chat {
 	constructor() {
 		super();
+		this.isGroup = true;
 
 		this.creator = '0';
 		this.createdAt = 0;
+
+		this.properties.push('creator');
+		this.properties.push('createdAt');
 	}
 }
 
