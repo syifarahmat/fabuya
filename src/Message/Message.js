3fab31d78feac0af53d1086d1d12df017e9137a4
HanzCEO <hanzhaxors@gmail.com>
2022-04-27 07:06:08 +0700

    feat(Message): Add `.senderName` and changed `.sender` to a person jid

--- src/Message/Message.js
+++ src/Message/Message.js
@@ -3,14 +3,17 @@ class Message {
 		this.raw = raw;
 		this.id = raw.key.id;
 		this.isMe = raw.key.fromMe;
+
 		this.from = raw.key.remoteJid;
+		this.sender = raw.key.participant || raw.key.remoteJid;
+		this.senderName = raw.pushName;
+
 		// TODO: this.chat
 		// TODO: this.from: User = null;
 		this.me = null;
 		this.chat = null;
 
 		this.timestamp = raw.messageTimestamp;
-		this.sender = this.pusher = raw.pushName;
 	}
 }
 
