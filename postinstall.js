c881a0e915e55a0c14c92188b172a761cf4e0224
HanzCEO <hanzhaxors@gmail.com>
2022-05-15 09:09:48 +0700

    chore(postinstall): Fixed looping issue

--- postinstall.js
+++ postinstall.js
@@ -1,12 +1,28 @@
 const fs = require('fs');
 const { spawnSync } = require('child_process');
 
+if (!fs.existsSync(__dirname + '/docs')) {
+	console.log('[i] Npm package detected, skipping post-install.');
+	process.exit(0);
+}
+
 // Initialize git submodule
-spawnSync('git', ['submodule',  'update', '--init', '-N', 'Baileys']);
-spawnSync('npm', ['install', '-D', '--prefix ./Baileys']);
+let opts = {stdio: ['pipe', process.stdio, process.stderr]}
+console.log('[i] Updating submodule');
+let res1 = spawnSync('git', ['submodule',  'update', '--init', '-N', 'Baileys'], opts);
+console.log('[i] Installing submodule dependencies on', __dirname + '/Baileys');
+let res2 = spawnSync('npm', ['install', '-D', '--prefix', __dirname + '/Baileys'], opts);
+console.dir(res2)
 
 // Move directories
-fs.renameSync('./Baileys/lib', './baileys-lib');
+let sd = (s, d) => ({source: s, dest: d});
+let dirs = [
+	sd(__dirname + '/Baileys/lib', __dirname + '/baileys-lib'),
+	sd(__dirname + '/Baileys/WAProto', __dirname + '/WAProto'),
+	sd(__dirname + '/Baileys/WASignalGroup', __dirname + '/WASignalGroup'),
+]
 
-fs.renameSync('./Baileys/WAProto', './WAProto');
-fs.renameSync('./Baileys/WASignalGroup', './WASignalGroup');
+for (const d of dirs) {
+	fs.rmSync(d.dest, {force: true, recursive: true});
+	fs.renameSync(d.source, d.dest);
+}
