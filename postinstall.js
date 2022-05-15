b4bd3dda28ddae213dcb4a78ec0bbd4074b29fec
HanzCEO <hanzhaxors@gmail.com>
2022-05-15 09:59:38 +0700

    chore(postinstall): Work around git submodule dependency

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const { spawnSync } = require('child_process');
 
+// TODO: Update BAILEYS_HASH automatically
+BAILEYS_HASH = '211a899ed4f56d85fc1acbb9439de8865ac4d777';
+
 if (!fs.existsSync(__dirname + '/docs')) {
 	console.log('[i] Npm package detected, skipping post-install.');
 	process.exit(0);
@@ -14,8 +17,9 @@ let opts = {
 
 console.log('[i] Initializing Git');
 spawnSync('git', ['init'], opts);
+spawnSync('git', ['submodule', 'add', 'https://github.com/adiwajshing/Baileys.git', 'Baileys'], opts);
 console.log('[i] Updating submodule');
-let res1 = spawnSync('git', ['submodule',  'update', '--init', '-N', 'Baileys'], opts);
+let res1 = spawnSync('git', ['update-index',  '--cache-info 160000', BAILEYS_HASH, 'Baileys'], opts);
 console.log('[i] Installing submodule dependencies on', __dirname + '/Baileys');
 let res2 = spawnSync('npm', ['install', '-D', '--prefix', __dirname + '/Baileys'], opts);
 console.dir(res2)
