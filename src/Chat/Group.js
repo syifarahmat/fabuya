bc7c9bbdd3499adf70cdf45c0945ea711eaa092c
HanzCEO <hanzhaxors@gmail.com>
2022-05-15 11:24:26 +0700

    feat(Group): Added ability to fetch metadata directly

--- src/Chat/Group.js
+++ src/Chat/Group.js
@@ -8,8 +8,25 @@ class Group extends Chat {
 		this.creator = '0';
 		this.createdAt = 0;
 
+		// TODO: desc
+		// TODO: isOnlyAdmins
+		// TODO: isSettingsOpen
+		// TODO: members getter
+
 		this.properties.push('creator');
 		this.properties.push('createdAt');
+	}
+
+	static async fromJid(sock, jid) {
+		let data = await sock.groupMetadata(jid);
+		let g = new Group();
+
+		g.id = data.id;
+		g.name = data.subject;
+		g.creator = data.owner;
+		g.createdAt = data.creation;
+
+		return g;
 	}
 }
 
