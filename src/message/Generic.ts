192ab920e1b2770a20cf3ce5af527a942d445baf
HanzCEO <hanzhaxors@gmail.com>
2022-05-26 16:35:53 +0700

    fix(Generic): Changed to Partial<T>

--- src/message/Generic.ts
+++ src/message/Generic.ts
@@ -1,4 +1,4 @@
 import { Message } from './Message'
 import { TextMessage } from './TextMessage'
 
-export interface GenericMessage extends Message, TextMessage {};
+export type GenericMessage = Partial<Message> & Partial<TextMessage>;
