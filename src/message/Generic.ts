02cc6e02bfcec3fd94cebe6fa50715e955665c8f
HanzCEO <hanzhaxors@gmail.com>
2022-06-08 06:18:33 +0700

    refactor(GenericMessage): Omitted ImageMessage

--- src/message/Generic.ts
+++ src/message/Generic.ts
@@ -1,10 +1,10 @@
 import { Message } from './Message'
 import { TextMessage } from './TextMessage'
-import { ImageMessage } from './ImageMessage'
+//import { ImageMessage } from './ImageMessage'
 import { DocumentMessage } from './DocumentMessage'
 
 export interface GenericMessage extends Partial<Message>,
 					Partial<TextMessage>,
-					Partial<ImageMessage>,
+//					Partial<ImageMessage>,
 					Partial<DocumentMessage>
 					{};
