0a31ab4a4db8a725bee9431dff154c521f656c8a
Hanz <hanz@godot.id>
2022-06-11 06:51:12 +0700

    feat(storage): Added Chat history sync to `structured`

--- src/storage/structured.ts
+++ src/storage/structured.ts
@@ -45,10 +45,14 @@ export class StructuredStorage {
 		client.on('contacts.upsert', contactUpsert.bind(this));
 		client.on('contacts.update', contactUpdate.bind(this));
 
-//		client.on('chats.set', chatSync.bind(this));
+		// Chat mutations
+		client.on('chats.set', chatSync.bind(this));
+		client.on('chats.upsert', chatUpsert.bind(this));
+		client.on('chats.update', chatUpdate.bind(this));
 //		client.on('messages.set', messageSync.bind(this));
 	}
 
+	// TODO: Convert this to fabuya's Contact
 	searchContactByJid(jid: string): Contact | undefined {
 		jid = jidNormalizedUser(jid);
 		let file = path.join(this.saveDir, subDirs.contact, flattenJid(jid));
@@ -59,9 +63,23 @@ export class StructuredStorage {
 
 		return data;
 	}
+
+	// TODO: Convert this to fabuya's Chat
+	searchChatByJid(jid: string): Chat {
+		jid = jidNormalizedUser(jid);
+		let file = path.join(this.saveDir, subDirs.chat, flattenJid(jid));
+		if (!fs.existsSync(file)) return undefined;
+
+		let raw = fs.readFileSync(file).toString();
+		let data = JSON.parse(raw);
+
+		return data;
+	}
 };
 
+// Contact history sync
 function contactSync({ contacts }: ContactSyncData) {
+	// TODO: Detect and remove deleted contact
 	for (const contact of contacts) {
 		let jid = jidNormalizedUser(contact.id);
 		let fname: string = flattenJid(jid);
@@ -89,14 +107,48 @@ function contactUpdate(contacts: Partial<Contact>[]) {
 	}
 }
 
+// Chat history sync
 function chatSync({ chats, isLatest }: ChatSyncData) {
+	if (isLatest) {
+		let files = fs.readdirSync(path.join(this.saveDir, subDirs.chat));
+		for (const each of files) {
+			fs.unlinkSync(path.join(this.saveDir, subDirs.chat, each));
+		}
+	}
+
 	for (const chat of chats) {
-		console.dir(chat);
+		fs.writeFileSync(
+			path.join(this.saveDir, subDirs.chat, flattenJid(jidNormalizedUser(chat.id))),
+			// XXX: JSON stringify slow
+			// TODO: Store this as binary
+			JSON.stringify(chat)
+		);
+	}
+}
+
+// Chat upsert
+function chatUpsert(chats: Chat[]) {
+	chatSync.call(this, {
+		chats,
+		isLatest: false
+	});
+}
+
+// Chat update
+function chatUpdate(chats: Partial<Chat>[]) {
+	for (const chat of chats) {
+		let data: Chat = this.searchChatByJid(chat.id) ?? {};
+
+		Object.assign(data, chat);
+		contactSync.call(this, {
+			contacts: [data]
+		});
 	}
 }
 
 function messageSync({ messages, isLatest }: MessageSyncData) {
 	for (const message of messages) {
+		console.log("MESSAGE");
 		console.dir(message);
 	}
 }
